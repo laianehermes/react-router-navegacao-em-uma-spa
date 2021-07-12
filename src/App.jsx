dcce35f2f030242d6339fb1715a1d735929ce885
lsouza <lsouza@luxfacta.com>
2021-07-12 09:12:32 -0300

    create navbar and 404 page

--- src/App.jsx
+++ src/App.jsx
@@ -4,16 +4,22 @@ import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 import Home from "./paginas/Home";
 import Sobre from "./paginas/Sobre";
+import Pagina404 from "./paginas/Pagina404";
+import Cabecalho from "./componentes/Cabecalho";
 
 function App() {
   return (
     <Router>
+      <Cabecalho />
       <Switch>
         <Route exact path='/'>
           <Home />
         </Route>
         <Route path='/sobre'>
           <Sobre />
+        </Route>
+        <Route>
+          <Pagina404 />
         </Route>
       </Switch>
     </Router>
